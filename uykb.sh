d3eaa17067a6162642781d2b780803bfbf1946e6
uykb <tuzhenqiang@gmail.com>
2022-05-25 03:46:27 +0800

    Update uykb.sh

--- uykb.sh
+++ uykb.sh
@@ -38,7 +38,7 @@ elif (($chosen==6));then
 elif (($chosen==7));then
         yum install -y yum-utils && yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo && yum makecache fast && yum -y install docker-ce && systemctl start docker && systemctl enable docker
 elif (($chosen==8));then
-        yum -y install ntpdate && timedatectl set-timezone Asia/Shanghai && ntpdate ntp1.aliyun.com && systemctl start supervisord ; systemctl disable firewalld ; systemctl stop firewalld ; yum -y update && yum -y upgrade
+        yum -y install ntpdate && timedatectl set-timezone Asia/Shanghai && ntpdate ntp1.aliyun.com && systemctl start supervisord;systemctl disable firewalld && systemctl stop firewalld && yum -y update && yum -y upgrade
 elif (($chosen==9));then
         wget -N --no-check-certificate git.io/v.sh && chmod +x v.sh && bash v.sh
 elif (($chosen==10));then
