2fbe0aa2501bf936c3f80e2a47bd3f0068cca62c
uykb <tuzhenqiang@gmail.com>
2022-05-25 06:18:47 +0800

    Update uykb.sh

--- uykb.sh
+++ uykb.sh
@@ -6,7 +6,7 @@ echo -e "
 项 目 地 址   https://github.com/uykb/lfx1848 
 博 客         www.lfx1848.cc
 "当前时间: $DATE"
-"服务器IP: $osip"
+echo $osip
 -------------------------多功能一键安装脚本---------------------------
         1. 安装 v2-ui
         2. 安装 BBR PLUS
